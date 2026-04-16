1ca324ce0638c0ebb126839d6fac2fa00f10ebe1
Lötgott <146114273+Loetgott@users.noreply.github.com>
2026-04-16 10:11:58 +0200

    first definition of a few messages

--- common/src/messages.rs
+++ common/src/messages.rs
@@ -4,5 +4,26 @@ use serde::{Deserialize, Serialize};
 pub enum TcpMessage {
     // TODO: alle messages, die über tcp ausgetauscht werden, also z.b. addDevice, etc.
     // über tcp nur das, was zeitunkritisch ist oder garantiert sicher ankommen muss
-    //
+    createFixture {
+        fixture_type: FixtureType,
+    },
+
+    createDevice {
+        start_channel: u16,
+        universe: usize,
+        name: String,
+    },
+    setDeviceAdress {
+        new_start_channel: u16,
+        new_universe: usize,
+    },
+    setDeviceFixture,
+    deleteDevice,
+    setDeviceProperty {
+        property: ,
+        value: ,
+    },
+
+    loadProject,
+    saveProject,
 }
